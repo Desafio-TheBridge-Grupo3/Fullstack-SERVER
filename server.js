3fcbac7fdfe4b74d0501d21192c89d331fd4dffe
Cristina Mateo <cris.mateo.99@gmail.com>
2024-01-09 13:44:38 +0100

    config básica de middlewares

--- server.js
+++ server.js
@@ -2,16 +2,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+//middelwares
+const error404 = require('./middlewares/error404')
+const morgan = require('./middlewares/morgan')
+
+// Logger
+app.use(morgan(':method :host :status :param[id] - :response-time ms :body'));
+
+
+/* //swagger- documentation
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+ */
 app.use(express.json()); // Habilito recepción de JSON en servidor
 
+//rutas
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 
-app.get("*",(req,res)=>{
-    res.status(404).send("Gatito triste - 404 not found");
-})
+//para rutas no existentes
+app.use('*',error404)
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)
