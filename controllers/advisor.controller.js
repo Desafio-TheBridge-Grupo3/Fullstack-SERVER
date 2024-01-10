90f7c7c71563b20502e0e4ed6ddb6c30fcb8c64a
juliaisasti <144235263+juliaisasti@users.noreply.github.com>
2024-01-10 22:16:50 +0100

    main errors fixed

--- controllers/advisor.controller.js
+++ controllers/advisor.controller.js
@@ -2,22 +2,16 @@ const Advisor = require("../models/advisor.model");
 
 // GET ADVISOR(S)
 const getAdvisor = async (req, res) => {
-    //console.log("---", req, "---")
-    const advisorUser = req.query.user;
-    if (advisorUser) {
+    const advisorUser = req.body.user;
+    try {
+      if (advisorUser) {
         const query = await Advisor.findOne({
             where: { user: advisorUser}
         });
         res.status(200).json(query)
-    } else {
-        try {
-            const query = await Advisor.findAll();
-            res.status(200).json(query);
-        } catch (error) {
-            console.log(error)
-            res.status(400).json({ error: error.message })
-        }
-    }
+    } } catch (error) {
+      res.status(400).json({ error: error.message })
+  }
 }
 
 // ADD ADVISOR
@@ -26,7 +20,6 @@ const createAdvisor = async (req, res) => {
       const query = await Advisor.create(req.body);
       res.status(201).json(query);
     } catch (error) {
-      console.log(error);
       res.status(400).json({ error: error.message });
     }
   };
@@ -35,11 +28,10 @@ const createAdvisor = async (req, res) => {
 const updateAdvisor = async (req, res) => {
     try {
       const update = await Advisor.update(req.body, {
-        where: { user: req.query.user },
+        where: { user: req.body.user },
       });
       res.status(200).json(update);
     } catch (error) {
-      console.log(error);
       res.status(400).json({ error: error.message });
     }
   };
@@ -48,11 +40,10 @@ const updateAdvisor = async (req, res) => {
 const deleteAdvisor = async (req, res) => {
     try {
       const deleted = await Advisor.destroy({
-        where: { user: req.query.user },
+        where: { user: req.body.user },
       });
       res.status(200).json(deleted);
     } catch (error) {
-      console.log(error);
       res.status(400).json({ error: error.message });
     }
   };
