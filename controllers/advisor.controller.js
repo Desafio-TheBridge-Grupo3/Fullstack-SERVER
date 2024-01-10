0b927a9bb3cafaf0ecfa4bb519ed8e864ec660a6
juliaisasti <144235263+juliaisasti@users.noreply.github.com>
2024-01-10 17:26:27 +0100

    advisor CRUD finished, not tested

---
+++ controllers/advisor.controller.js
@@ -0,0 +1,67 @@
+const Advisor = require("../models/advisor.model");
+
+// GET ADVISOR(S)
+const getAdvisor = async (req, res) => {
+    //console.log("---", req, "---")
+    const advisorUser = req.query.user;
+    if (advisorUser) {
+        const query = await Advisor.findOne({
+            where: { user: advisorUser}
+        });
+        res.status(200).json(query)
+    } else {
+        try {
+            const query = await Advisor.findAll();
+            res.status(200).json(query);
+        } catch (error) {
+            console.log(error)
+            res.status(400).json({ error: error.message })
+        }
+    }
+}
+
+// ADD ADVISOR
+const createAdvisor = async (req, res) => {
+    try {
+      const query = await Advisor.create(req.body);
+      res.status(201).json(query);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ error: error.message });
+    }
+  };
+
+  // UPDATE PLANT
+const updateAdvisor = async (req, res) => {
+    try {
+      const update = await Advisor.update(req.body, {
+        where: { user: req.query.user },
+      });
+      res.status(200).json(update);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ error: error.message });
+    }
+  };
+
+  // DELETE ADVISOR
+const deleteAdvisor = async (req, res) => {
+    try {
+      const deleted = await Advisor.destroy({
+        where: { user: req.query.user },
+      });
+      res.status(200).json(deleted);
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ error: error.message });
+    }
+  };
+
+  const plantsController = {
+    getAdvisor,
+    createAdvisor,
+    updateAdvisor,
+    deleteAdvisor,
+  };
+  
+  module.exports = plantsController;
