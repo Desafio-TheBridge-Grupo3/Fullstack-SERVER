7634879a18e8a419463fabc4de62b679241f874d
juliaisasti <144235263+juliaisasti@users.noreply.github.com>
2024-01-10 22:19:35 +0100

    main errors fixed

--- controllers/address.controller.js
+++ controllers/address.controller.js
@@ -2,66 +2,62 @@ const Address = require("../models/address.model");
 
 // GET ADVISOR(S)
 const getAddress = async (req, res) => {
-    //console.log("---", req, "---")
-    const addressId = req.query.id;
+  const addressId = req.body.id;
+  try {
     if (addressId) {
-        const query = await Address.findOne({
-            where: { id: addressId }
-        });
-        res.status(200).json(query)
-    } else {
-        try {
-            const query = await Address.findAll();
-            res.status(200).json(query);
-        } catch (error) {
-            console.log(error)
-            res.status(400).json({ error: error.message })
-        }
+      const query = await Address.findOne({
+        where: { id: addressId },
+      });
+      res.status(200).json(query);
     }
-}
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
+  }
+};
 
 // ADD ADVISOR
 const createAddress = async (req, res) => {
-    try {
-      const query = await Address.create(req.body);
-      res.status(201).json(query);
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({ error: error.message });
-    }
-  };
+  try {
+    const query = await Address.create(req.body);
+    res.status(201).json(query);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
+  }
+};
 
-  // UPDATE PLANT
+// UPDATE PLANT
 const updateAddress = async (req, res) => {
-    try {
-      const update = await Address.update(req.body, {
-        where: { id: req.query.id },
-      });
-      res.status(200).json(update);
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({ error: error.message });
-    }
-  };
+  try {
+    const update = await Address.update(req.body, {
+      where: { id: req.body.id },
+    });
+    res.status(200).json(update);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
+  }
+};
 
-  // DELETE ADVISOR
+// DELETE ADVISOR
 const deleteAdvisor = async (req, res) => {
-    try {
-      const deleted = await Address.destroy({
-        where: { id: req.query.id },
-      });
-      res.status(200).json(deleted);
-    } catch (error) {
-      console.log(error);
-      res.status(400).json({ error: error.message });
-    }
-  };
+  try {
+    const deleted = await Address.destroy({
+      where: { id: req.body.id },
+    });
+    res.status(200).json(deleted);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
+  }
+};
 
-  const plantsController = {
-    getAddress,
-    createAddress,
-    updateAddress,
-    deleteAdvisor,
-  };
-  
-  module.exports = plantsController;
+const addressController = {
+  getAddress,
+  createAddress,
+  updateAddress,
+  deleteAdvisor,
+};
+
+module.exports = addressController;
