4467fc7629f39f4c306f03382c6cf63d28134564
Gregorio <145268234+GREGORIOtsr@users.noreply.github.com>
2024-01-14 09:25:11 +0100

    added ssl option to db conecction

--- config/sqlConnection.js
+++ config/sqlConnection.js
@@ -4,9 +4,13 @@ require('dotenv').config();
 const db = new Sequelize(process.env.SQL_DATABASE, process.env.SQL_USER, `${process.env.SQL_PWD}`, {
     host: process.env.SQL_HOST,
     dialect: 'postgres',
+    ssl: true,
     define: {
         freezeTableName: true,
         timestamps: false,
+    },
+    dialectOptions: {
+        ssl: true,
     }
 });
 
