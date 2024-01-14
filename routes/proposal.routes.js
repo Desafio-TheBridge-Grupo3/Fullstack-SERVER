91451bb4e6eb691228cd70decd96f93a76653df1
Gregorio <145268234+GREGORIOtsr@users.noreply.github.com>
2024-01-14 19:34:45 +0100

    refactored models names, body read by controllers and db connection options

--- routes/proposal.routes.js
+++ routes/proposal.routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('../controllers/proposal.controllers');
 
-router.get('/:id', controllers.getProposal);
-router.get('/', controllers.getAllProposals);
+router.get('/', controllers.getProposal);
+router.get('/all', controllers.getAllProposals);
 router.post('/', controllers.createProposal);
 router.delete('/', controllers.deleteProposal);
 
