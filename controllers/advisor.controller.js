36d1f29da7ba6ec2927c620ea2ad863fcdac5319
Gregorio <145268234+GREGORIOtsr@users.noreply.github.com>
2024-01-15 17:03:43 +0100

    changed cookies settings and advisor model and controllers plus minor fixes

--- controllers/advisor.controller.js
+++ controllers/advisor.controller.js
@@ -1,58 +1,166 @@
+const Sequelize = require('sequelize');
+const op = Sequelize.Op;
 const Advisor = require("../models/advisor.model");
 
-// GET ADVISOR(S)
 const getAdvisor = async (req, res) => {
-    const advisorUser = req.body.user;
-    try {
-      if (advisorUser) {
-        const query = await Advisor.findOne({
-            where: { user: advisorUser}
+  try {
+    const username = req.query.username || '';
+    const email = req.query.email || '';
+    const advisor = await Advisor.findOne({
+      attributes: {
+        exclude: ["id", "password"],
+      },
+      where: {
+        [op.or]: {
+          username: username,
+          email: email,
+        },
+      },
+    });
+    if (!advisor && username) {
+      res
+        .status(200)
+        .json({
+          success: false,
+          message: `Advisor ${username} does not exist.`,
         });
-        res.status(200).json(query)
-    } } catch (error) {
-      res.status(400).json({ error: error.message })
+    } else if (!advisor && email) {
+      res
+        .status(200)
+        .json({
+          success: false,
+          message: `Advisor with email ${email} does not exist.`,
+        });
+    } else {
+      res.status(200).json({ success: true, data: advisor });
+    }
+  } catch (error) {
+    res.status(400).json({ message: `ERROR: ${error.stack}` });
   }
-}
+};
 
-// ADD ADVISOR
 const createAdvisor = async (req, res) => {
-    try {
-      const query = await Advisor.create(req.body);
-      res.status(201).json(query);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
+  try {
+    const username = req.body.username;
+    const email = req.body.email;
+    let advisor = await Advisor.findOne({
+      attributes: {
+        exclude: ["id", "password"],
+      },
+      where: { username: username },
+    });
+    if (advisor) {
+      res
+        .status(200)
+        .json({ success: false, message: `Username already in use.` });
+    } else {
+      advisor = await Advisor.findOne({
+        attributes: {
+          exclude: ["id", "password"],
+        },
+        where: { email: email },
+      });
+      if (advisor) {
+        res
+          .status(200)
+          .json({ success: false, message: `Email already registered.` });
+      } else {
+        advisor = await Advisor.create(req.body);
+        res.status(201).json({ success: true, user: advisor });
+      }
     }
-  };
+  } catch (error) {
+    res.status(400).json({ message: `ERROR: ${error.stack}` });
+  }
+};
 
-  // UPDATE PLANT
 const updateAdvisor = async (req, res) => {
-    try {
-      const update = await Advisor.update(req.body, {
-        where: { user: req.body.user },
-      });
-      res.status(200).json(update);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  };
+  try {
 
-  // DELETE ADVISOR
+    let noUser = true;
+    let noEmail = true;
+
+    if (req.body.username) {
+      let advisor = await Advisor.findOne({
+        attributes: {
+          exclude: ["id", "password"],
+        },
+        where: { username: req.body.username },
+      });
+      if (advisor) {noUser = false}
+    }
+    
+    if (req.body.email) {
+      let advisor = await Advisor.findOne({
+        attributes: {
+          exclude: ["id", "password"],
+        },
+        where: { email: req.body.email },
+      });
+      if (advisor) {noEmail = false}
+    };
+    console.log('----------------------------------------');
+    console.log(noUser && noEmail);
+    console.log('----------------------------------------');
+    if (noUser && noEmail) {
+      await Advisor.update(req.body, {
+        where: {email: req.params.email},
+        individualHooks: true,
+      });
+      res.status(200).json({success: true, message: `Advisor register updated successfully.`});
+    } else {
+      res.status(200).json({ success: false, message: `Email or username already registered.` });
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: `ERROR: ${error.stack}` });
+  }
+};
+
 const deleteAdvisor = async (req, res) => {
-    try {
-      const deleted = await Advisor.destroy({
-        where: { user: req.body.user },
-      });
-      res.status(200).json(deleted);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
+  try {
+    const username = req.query.username || '';
+    const email = req.query.email || '';
+    const advisor = await Advisor.findOne({
+      attributes: {
+        exclude: ["password"],
+      },
+      where: {
+        [op.or]: {
+          username: username,
+          email: email,
+        },
+      },
+    });
+    if (!advisor && username) {
+      res
+        .status(200)
+        .json({
+          success: false,
+          message: `Advisor ${username} does not exist.`,
+        });
+    } else if (!advisor && email) {
+      res
+        .status(200)
+        .json({
+          success: false,
+          message: `Advisor with email ${email} does not exist.`,
+        });
+    } else {
+      await advisor.destroy();
+      res.status(200).json({success: true, message: `Advisor register deleted successfully.`});
     }
-  };
+  } catch (error) {
+    res.status(400).json({ message: `ERROR: ${error.stack}` });
+  }
+};
 
-  const advisorController = {
-    getAdvisor,
-    createAdvisor,
-    updateAdvisor,
-    deleteAdvisor,
-  };
-  
-  module.exports = advisorController;
+const controllers = {
+  getAdvisor,
+  createAdvisor,
+  updateAdvisor,
+  deleteAdvisor,
+};
+
+module.exports = controllers;
